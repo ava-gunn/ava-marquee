a46dba82e25d4a94c32ecbf689b8993aa5191d4a
Clark Gunn <clark.gunn@formidable.com>
2025-02-27 23:27:38 -0500

    add vertical mode

--- src/ava-marquee.ts
+++ src/ava-marquee.ts
@@ -7,23 +7,49 @@ export class AvaMarquee extends LitElement {
     :host {
       display: block;
       overflow: hidden;
-      position: relative;
-      width: 100%;
+      white-space: nowrap;
+    }
+
+    :host([direction='vertical']) {
+      height: var(--height, 100%);
+
+      .container {
+        padding-left: 0;
+        padding-top: 100%;
+        display: block;
+        writing-mode: vertical-lr;
+      }
     }
 
     .container {
       display: inline-block;
-      white-space: nowrap;
-      animation: scroll linear infinite;
-      animation-duration: var(--scroll-speed, 60s);
     }
 
-    @keyframes scroll {
-      from {
-        transform: translateX(100%);
+    @media (prefers-reduced-motion: no-preference) {
+      .container {
+        padding-left: 100%;
+        animation: var(--animation-name, scroll-horizontal) var(--speed, 60s)
+          linear infinite;
       }
+
+      :host([direction='vertical']) .container {
+        animation-name: scroll-vertical;
+      }
+    }
+
+    .container:hover {
+      animation-play-state: paused;
+    }
+
+    @keyframes scroll-horizontal {
       to {
         transform: translateX(-100%);
+      }
+    }
+
+    @keyframes scroll-vertical {
+      to {
+        transform: translateY(-100%);
       }
     }
   `;
